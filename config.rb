8b5cc24a977fe60567917f9a2aa27b1658eff214
Eugene Varlamov <varlamoved@gmail.com>
2017-02-26 00:45:08 +0300

    update docs

--- config.rb
+++ config.rb
@@ -9,6 +9,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+
+set :build_dir, 'docs'
+
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
